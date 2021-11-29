dc260272c43ae1de7d220df448ae214dc2ee2ded
mrlomdom <dinhquangtientb@gmail.com>
2021-11-29 09:57:30 +0700

    Nạp dự án lên github

---
+++ Assets/Scripts/MonoBehaviours/Consumable.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+public class Consumable : MonoBehaviour 
+{
+    public Item item;
+}
---
+++ Assets/Scripts/Scriptable Objects/HitPoints.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+//1
+[CreateAssetMenu(menuName = "HitPoints")]
+public class HitPoints : ScriptableObject
+{
+//2
+    public float value;
+}
